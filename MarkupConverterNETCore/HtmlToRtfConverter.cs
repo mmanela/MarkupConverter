7e917f764fccd95b24cc219b60c862e2f398fd4e
danjhetherington0 <danjhetherington0@gmail.com>
2021-05-10 17:23:18 +0100

    Added .NET Core 3.1 Verson

---
+++ MarkupConverterNETCore/HtmlToRtfConverter.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace MarkupConverter
+{
+    public static class HtmlToRtfConverter
+    {
+
+        public static string ConvertHtmlToRtf(string htmlText)
+        {
+            var xamlText = HtmlToXamlConverter.ConvertHtmlToXaml(htmlText, false);
+
+            return ConvertXamlToRtf(xamlText);
+        }
+
+        private static string ConvertXamlToRtf(string xamlText)
+        {
+            if (string.IsNullOrEmpty(xamlText)) return "";
+            var flowDocument = new FlowDocument();
+            
+
+            var textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+
+            //Create a MemoryStream of the xaml content
+
+            using (var xamlMemoryStream = new MemoryStream())
+            {
+                using (var xamlStreamWriter = new StreamWriter(xamlMemoryStream))
+                {
+                    xamlStreamWriter.Write(xamlText);
+                    xamlStreamWriter.Flush();
+                    xamlMemoryStream.Seek(0, SeekOrigin.Begin);
+
+                    //Load the MemoryStream into TextRange ranging from start to end of RichTextBox.
+                    textRange.Load(xamlMemoryStream, DataFormats.Xaml);
+                }
+            }
+
+            using (var rtfMemoryStream = new MemoryStream())
+            {
+
+                textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+                textRange.Save(rtfMemoryStream, DataFormats.Rtf);
+                rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                using (var rtfStreamReader = new StreamReader(rtfMemoryStream))
+                {
+                    return rtfStreamReader.ReadToEnd();
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ MarkupConverterNETCore/RtfToHtmlConverter.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace MarkupConverter
+{
+    public static class RtfToHtmlConverter
+    {
+        private const string FlowDocumentFormat = "<FlowDocument>{0}</FlowDocument>";
+
+        public static string ConvertRtfToHtml(string rtfText)
+        {
+            var xamlText = string.Format(FlowDocumentFormat, ConvertRtfToXaml(rtfText));
+
+            return HtmlFromXamlConverter.ConvertXamlToHtml(xamlText, false);
+        }
+
+        private static string ConvertRtfToXaml(string rtfText)
+        {
+
+            if (string.IsNullOrEmpty(rtfText)) return "";
+            var flowDocument = new FlowDocument();
+            var textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+            //Create a MemoryStream of the Rtf content
+
+            using (var rtfMemoryStream = new MemoryStream())
+            {
+                using (var rtfStreamWriter = new StreamWriter(rtfMemoryStream))
+                {
+                    rtfStreamWriter.Write(rtfText);
+                    rtfStreamWriter.Flush();
+                    rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+
+                    //Load the MemoryStream into TextRange ranging from start to end of RichTextBox.
+                    textRange.Load(rtfMemoryStream, DataFormats.Rtf);
+                }
+            }
+
+            using (var rtfMemoryStream = new MemoryStream())
+            {
+
+                textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+                textRange.Save(rtfMemoryStream, DataFormats.Xaml);
+                rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                using (var rtfStreamReader = new StreamReader(rtfMemoryStream))
+                {
+                    return rtfStreamReader.ReadToEnd();
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
